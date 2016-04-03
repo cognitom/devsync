6c68bfcff3b1c9796f6e34b39d086d9461e560a0
Tsutomu Kawamura <cognitom@gmail.com>
2016-04-03 18:34:19 +0900

    Rewrites into generator and yield

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
 const
+  co = require('co'),
   path = require('path'),
   fsp = require('fs-promise'),
   globby = require('globby'),
   spawn = require('./lib/spawn'),
   which = require('./lib/which')
 
-module.exports = (options) => {
+module.exports = co.wrap(function* (options) {
   const
     cwd = path.resolve(process.cwd(), options.cwd),
-    target = path.resolve(process.cwd(), options.target)
+    target = path.resolve(process.cwd(), options.target),
+    rsyncPath = yield which('rsync'),
+    metafile = path.join(cwd, 'package.json'),
+    metatext = yield fsp.readFile(metafile, 'utf8'),
+    meta = JSON.parse(metatext),
+    patterns = [
+      `node_modules/${ meta.name }`, // checks children
+      `*/node_modules/${ meta.name }`, // checks grandchildren
+    ],
+    paths = yield globby(patterns, { cwd: target }),
+    installed = paths.map(dir => path.resolve(target, dir)),
+    args = [
+      '--recursive', // recurse into directories
+      '--delete', // delete extraneous files from dest dirs
+      '--delete-excluded', // also delete excluded files from dest dirs
+      '--cvs-exclude', // auto-ignore files in the same way CVS does
+      '--exclude=node_modules',
+      `${ cwd }/`
+    ]
 
-  var installed = []
-  return fsp.readFile(path.join(cwd, 'package.json'), 'utf8')
-    .then(metatext => {
-      const
-        meta = JSON.parse(metatext)
-        patterns = [
-          `node_modules/${ meta.name }`, // checks children
-          `*/node_modules/${ meta.name }`, // checks grandchildren
-        ]
-      return globby(patterns, { cwd: target })
-    })
-    .then(paths => {
-      installed = paths.map(dir => path.resolve(target, dir))
-      return which('rsync')
-    })
-    .then(rsyncPath => Promise.all(installed.map(dir => {
-      var args = [
-        '--recursive', // recurse into directories
-        '--delete', // delete extraneous files from dest dirs
-        '--delete-excluded', // also delete excluded files from dest dirs
-        '--cvs-exclude', // auto-ignore files in the same way CVS does
-        '--exclude=node_modules',
-        `${ cwd }/`,
-        `${ dir }/`
-      ]
+  for (var dir of installed) {
+    try {
+      yield spawn(rsyncPath, args.concat(`${ dir }/`))
       console.log(dir)
-      return spawn(rsyncPath, args)
-    })))
-    .catch(err => {
+    } catch (err) {
       console.log(err.toString())
-    })
-}
+    }
+  }
+})
